5779d04dc92063b462d23aaf229e88018919899a
Robert Molina <rmolinamir@gmail.com>
2019-09-17 22:42:32 -0400

    open source caption

--- src/components/Homepage/OpenSource/index.js
+++ src/components/Homepage/OpenSource/index.js
@@ -4,11 +4,17 @@ import React from 'react';
 // Components
 import { Container } from 'components/UI';
 import { H3, P } from 'components/UI/Text';
+import SectionCaption from '../SectionCaption';
 
-export default function TLDR() {
+export default function OpenSource() {
   return (
     <Container id="open-source">
       <H3 color="primary"><strong>Open Source</strong></H3>
+      <SectionCaption>
+        Open Source projects is something I constantly keep an eye on. I&apos;ve found out that contributing to these is a
+        great way of learning. And on top of that, I&apos;ve also done a few open source projects hosted on NPM or GitHub.
+        From complete write-ups to React.js components, you may click on the links below for more details of each of them!
+      </SectionCaption>
       <br />
       <P>Technologies: </P>
       <P>Work: </P>
