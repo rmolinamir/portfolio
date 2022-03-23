99da68f627b1e644af98e65e53191e823127e01b
Robert Molina <rmolinamir@gmail.com>
2022-03-23 11:57:40 -0400

    feat(introduction): improved introduction content

--- src/components/Homepage/Introduction/index.js
+++ src/components/Homepage/Introduction/index.js
@@ -14,19 +14,19 @@ export default function Introduction() {
         <br />
         <P>
           My career started with a major in mechanical engineering. I have been programming professionally for over {amountOfYears} years
-          in a variety of technologies and languages, including TypeScript, Solidity, C#, Python, relational databases,
+          in a variety of technologies and languages, including TypeScript, C#, Python, relational databases,
           document-oriented databases, ORMs, frameworks, and much more.
-        </P>
-        <br />
-        <P>
-          My current interest is in software architecture, backend development, and the blockchain space. My background includes developing web
-          apps using TypeScript, React, Next.js, among other frontend oriented frameworks, and backend APIs using a variety of languages, technologies,
-          and backend oriented frameworks such as Express and NodeJS.
         </P>
         <br />
         <P>
           As a Software Engineer, I design and develop highly scalable, secure and reliable software systems. I focus on the core domain
           of the application and create the services that map most closely to the concerns of the domain experts.
+        </P>
+        <br />
+        <P>
+          My current interests are software architecture, backend development, domain-driven design, event sourcing, and most recently the blockchain space. My background includes developing web
+          apps using TypeScript, React, Next.js, among other frontend oriented frameworks, and backend APIs using a variety of languages, technologies,
+          and backend oriented frameworks such as Node.js, Express, NestJS, .NET, et al.
         </P>
       </Container>
     </Animations.PopIn>
